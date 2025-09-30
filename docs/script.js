929538acf8f616a3c040c803be8a9493bf2c990e
eXioStorm <lunardsi@gmail.com>
2025-09-30 13:01:04 +1000

    more pages setup~

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,6 @@
 const toggleButton = document.getElementById('toggle-btn')
 const sidebar = document.getElementById('sidebar')
 
-if(sidebar.classList.contains('close')){
-  toggleButton.classList.add('rotate')
-}
-
 function toggleSidebar(){
   sidebar.classList.toggle('close')
   toggleButton.classList.toggle('rotate')
